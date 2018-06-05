ae181de7a9f45e5536c12d8a44661fa46e905a0d
mountainflo <tobusch@hm.edu>
2018-06-05 22:37:46 +0200

    create query for graphql reading type and responding an item

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ## Please note
 
 You have to run the whole project not only the main function!
+
+Input:
+
+```
+{
+  calculate(type: RENTAL) {
+    id
+    name
+    discout_perc
+  }
+}
+
+
+``
--- pricecalculation.go
+++ pricecalculation.go
@@ -5,10 +5,18 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
-type user struct {
-	ID   string
+type Item struct {
+	ID   int
 	Name string
+	discout_perc float32
 }
+
+type CalcType int
+
+const (
+	RENTAL    CalcType = 0
+	LEASING   CalcType = 1
+)
 
 var (
 	pricecalculationSchema graphql.Schema
@@ -16,27 +24,43 @@ var (
 
 func initGraphQl(){
 
-	//for converting the input to a user object
-	var userType = graphql.NewObject(
-		graphql.ObjectConfig{
-			Name: "User",
+	/*
+		for converting the input to an Item object
+
+		input Item {
+  			id: Int
+  			name: String
+		}
+	 */
+	var item = graphql.NewObject(graphql.ObjectConfig{
+			Name: "item",
 			Fields: graphql.Fields{
 				"id": &graphql.Field{
-					Type: graphql.NewNonNull(graphql.Int),
+					Type: graphql.NewNonNull(graphql.Int),	//NewNonNull if really necessary
 					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-						user, ok := p.Source.(user)
+						item, ok := p.Source.(Item)
 						if ok {
-							return user.ID, nil
+							return item.ID, nil
 						}
 						return nil, nil
 					},
 				},
 				"name": &graphql.Field{
-					Type: graphql.NewNonNull(graphql.Int),
+					Type: graphql.String,
 					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-						user, ok := p.Source.(user)
+						item, ok := p.Source.(Item)
 						if ok {
-							return user.Name, nil
+							return item.Name, nil
+						}
+						return nil, nil
+					},
+				},
+				"discout_perc": &graphql.Field{
+					Type: graphql.Float,
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						item, ok := p.Source.(Item)
+						if ok {
+							return item.discout_perc, nil
 						}
 						return nil, nil
 					},
@@ -45,21 +69,52 @@ func initGraphQl(){
 		},
 	)
 
-	var queryType = graphql.NewObject(
-		graphql.ObjectConfig{
+	/*
+		enum CalcType {
+		  RENTAL
+		  LEASING
+		}
+	 */
+	calcTypeEnum := graphql.NewEnum(graphql.EnumConfig{
+		Name:        "CalcType",
+		Description: "The type for the calculation.",
+		Values: graphql.EnumValueConfigMap{
+			"RENTAL": &graphql.EnumValueConfig{
+				Value:       RENTAL,
+			},
+			"LEASING": &graphql.EnumValueConfig{
+				Value:       LEASING,
+			},
+		},
+	})
+
+	/*
+		type RootQuery {
+			calculate(type: CalcType, items [Item], family_discount: Int): [Item]
+		}
+	 */
+	var rootQuery = graphql.NewObject(graphql.ObjectConfig{
 			Name: "Query",
 			Fields: graphql.Fields{
-				"user": &graphql.Field{
-					Type: userType,
+				"calculate": &graphql.Field{
+					Type: item,	//result of the request
+					//Type: graphql.NewList(Item),	//result of the request
 					Args: graphql.FieldConfigArgument{
-						"id": &graphql.ArgumentConfig{
-							Type: graphql.String,
+						"type": &graphql.ArgumentConfig{
+							Type: calcTypeEnum,
 						},
+						/*"item": &graphql.ArgumentConfig{
+							Type: item,
+							//Type: graphql.NewList(Item),
+						},*/
 					},
 					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-						idQuery, isOK := p.Args["id"].(string)
-						if isOK {
-							return doSomethingFancy(idQuery)
+						//get the values from the request
+						typeQuery, typeIsOK := p.Args["type"].(CalcType)
+						//itemQuery, itemIsOK := p.Args["Item"].(Item)
+						if typeIsOK {
+							//do the calculation it input was correct
+							return calculatePriceForItem(typeQuery)
 						}
 						return nil, nil
 					},
@@ -67,13 +122,16 @@ func initGraphQl(){
 			},
 		})
 
-
+	//definition of the API
 	pricecalculationSchema, _ = graphql.NewSchema(graphql.SchemaConfig{
-		Query: queryType,
+		Query: rootQuery,
 	})
 }
 
-//function that does something with the id the client send use
-func doSomethingFancy(idQuery string) (user, error) {
-	return user{ID:idQuery, Name:"Hello World"}, nil
+func calculatePriceForItem(typeQuery CalcType) (Item, error) {
+
+	//TODO calculate price
+
+	//return Item{ID:itemQuery.ID, Name:itemQuery.Name, discout_perc:itemQuery.discout_perc}, nil
+	return Item{ID:1, Name:"asdf", discout_perc:1.5}, nil
 }
