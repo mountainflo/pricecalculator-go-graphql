56ba03c06547aaca576c3bdea18f05971fe3bf0c
mountainflo <tobusch@hm.edu>
2018-06-06 14:45:15 +0200

    Use structs as input Requests can contain structs. They will send back as response. Solution uses Marshal and Unmarshal-function

--- pricecalculation.go
+++ pricecalculation.go
@@ -3,12 +3,18 @@ package main
 
 import (
 	"github.com/graphql-go/graphql"
+	"encoding/json"
 )
 
 type Item struct {
-	ID   int
-	Name string
+	id   int
+	name string
 	discout_perc float32
+}
+
+type Blah struct {
+	Id   string   `json:"id"`
+	Name   string `json:"name"`
 }
 
 type CalcType int
@@ -32,7 +38,7 @@ func initGraphQl(){
   			name: String
 		}
 	 */
-	var item = graphql.NewObject(graphql.ObjectConfig{
+	/*var item = graphql.NewObject(graphql.ObjectConfig{
 			Name: "item",
 			Fields: graphql.Fields{
 				"id": &graphql.Field{
@@ -40,7 +46,7 @@ func initGraphQl(){
 					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 						item, ok := p.Source.(Item)
 						if ok {
-							return item.ID, nil
+							return item.id, nil
 						}
 						return nil, nil
 					},
@@ -50,7 +56,7 @@ func initGraphQl(){
 					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 						item, ok := p.Source.(Item)
 						if ok {
-							return item.Name, nil
+							return item.name, nil
 						}
 						return nil, nil
 					},
@@ -67,7 +73,58 @@ func initGraphQl(){
 				},
 			},
 		},
+	)*/
+
+	var blah = graphql.NewObject(graphql.ObjectConfig{
+		Name: "blah",
+		Fields: graphql.Fields{
+			"Id": &graphql.Field{
+				Type: graphql.String,	//NewNonNull if really necessary
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					blah, ok := p.Source.(Blah)
+					if ok {
+						return blah.Id, nil
+					}
+					return "ERROR", nil
+				},
+			},
+			"Name": &graphql.Field{
+				Type: graphql.String,	//NewNonNull if really necessary
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					blah, ok := p.Source.(Blah)
+					if ok {
+						return blah.Name, nil
+					}
+					return "ERROR", nil
+				},
+			},
+		},
+	},
 	)
+
+	var blahInput = graphql.NewInputObject(
+		graphql.InputObjectConfig{
+			Name: "MyInputType",
+			Fields: graphql.InputObjectConfigFieldMap{
+				"Id": &graphql.InputObjectFieldConfig{
+					Type: graphql.String,
+				},
+				"Name": &graphql.InputObjectFieldConfig{
+					Type: graphql.String,
+				},
+			},
+		},
+	)
+
+	/*var blahInput = graphql.NewInputObject(graphql.InputObjectConfig{
+			Name: "MyInputType",
+			Fields: graphql.InputObjectConfigFieldMap{
+				"key": &graphql.InputObjectFieldConfig{
+					Type: graphql.String,
+				},
+			},
+		},
+		)*/
 
 	/*
 		enum CalcType {
@@ -97,24 +154,31 @@ func initGraphQl(){
 			Name: "Query",
 			Fields: graphql.Fields{
 				"calculate": &graphql.Field{
-					Type: item,	//result of the request
+					Type: blah,	//result of the request
 					//Type: graphql.NewList(Item),	//result of the request
 					Args: graphql.FieldConfigArgument{
 						"type": &graphql.ArgumentConfig{
 							Type: calcTypeEnum,
 						},
-						/*"item": &graphql.ArgumentConfig{
-							Type: item,
+						"blah": &graphql.ArgumentConfig{
+							Type: blahInput,
 							//Type: graphql.NewList(Item),
-						},*/
+						},
 					},
 					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 						//get the values from the request
 						typeQuery, typeIsOK := p.Args["type"].(CalcType)
-						//itemQuery, itemIsOK := p.Args["Item"].(Item)
-						if typeIsOK {
+
+						//print input of graphql
+						rJSON, _ := json.Marshal(p.Args["blah"])
+						result := Blah{}
+						err := json.Unmarshal(rJSON, &result)
+						print("result.Id: ")
+						println(result.Id)
+
+						if typeIsOK && err == nil {
 							//do the calculation it input was correct
-							return calculatePriceForItem(typeQuery)
+							return calculatePriceForItem(typeQuery, result)
 						}
 						return nil, nil
 					},
@@ -128,10 +192,25 @@ func initGraphQl(){
 	})
 }
 
-func calculatePriceForItem(typeQuery CalcType) (Item, error) {
+func calculatePriceForItem(typeQuery CalcType, itemQuery Blah) (interface{}, error) {
 
 	//TODO calculate price
 
+	//print(itemQuery)
+
 	//return Item{ID:itemQuery.ID, Name:itemQuery.Name, discout_perc:itemQuery.discout_perc}, nil
-	return Item{ID:1, Name:"asdf", discout_perc:1.5}, nil
+	return Blah{Id:itemQuery.Id, Name:itemQuery.Name}, nil
 }
+
+
+/*
+	TEST with
+
+		{
+		  calculate(type: RENTAL, blah: {Id: "blahContent", Name: "blahNameConten"}) {
+			Id
+			Name
+		  }
+		}
+
+ */
